a0af3d30ed43119a7ea9e3bb2438dbef02765920
aprillianurazizah <aprillia.azizah1@gmail.com>
2023-06-04 23:10:25 +0700

    restyle homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -16,31 +16,30 @@ const HomePage = () => {
       <Carousel />
       <section className="mb-4">
         <div
-          className="grid grid-cols-2 w-full m-7"
+          className="grid grid-cols-1 sm:grid-cols-2 m-7 w-[95%]"
           style={{ backgroundColor: "#f1f1e8" }}
         >
-          <div className="text-center pl-7 pr-7">
+          <div className="text-center sm:text-center md:text-center pl-7 pr-7 ">
             <div className="mt-20">
-              <p class=" text-2xl">Welcome To</p>
-              <p class="text-2xl">The Art and History</p>
-              <p class="text-2xl">Ozeum Gallery</p>
-              <p className=" text-sm mt-3 p-5">
-                {" "}
+              <p className="text-2xl">Welcome To</p>
+              <p className="text-2xl">The Art and History</p>
+              <p className="text-2xl">Ozeum Gallery</p>
+              <p className="text-sm mt-3 p-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
                 nisi possimus neque! Animi, nulla sapiente, deserunt asperiores
                 eos eveniet voluptatem dolorum impedit est molestiae dicta
                 temporibus quia perferendis consectetur ab.
               </p>
-              <div className="flex gap-2 justify-center hover:scale-110">
+              <div className="flex items-center sm:items-center gap-2 sm:justify-center md:justify-center hover:scale-110 pb-5">
                 <button className="text-sm mt-1">Read More</button>
-                <BsArrowRight className=" mt-2 cursor-pointer" />
+                <BsArrowRight className="mt-2 cursor-pointer" />
               </div>
             </div>
           </div>
-          <div className="pr-7">
+          <div className=" w-[100%]">
             <img
               src="https://raw.githubusercontent.com/aprillianurazizah/right-image/main/right-img.png"
-              className=" w-[95%]"
+              className="w-full sm:w-[50%] md:w-[100%] mx-auto h-full"
               alt="Leonardo da Vinci"
             />
           </div>
